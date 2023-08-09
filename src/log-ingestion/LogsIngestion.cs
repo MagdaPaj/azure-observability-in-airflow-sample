6f1299527e4a2f4f50f4eb6200cac0d90cf3c770
Alexander Gassmann <agassman@microsoft.com>
2023-08-09 12:26:33 +0200

    minor refactoring of ingestion function

--- src/log-ingestion/LogsIngestion.cs
+++ src/log-ingestion/LogsIngestion.cs
@@ -15,7 +15,7 @@ namespace Sample.Function
         [FunctionName("LogsIngestion")]
         public async Task Run([BlobTrigger("airflowlogscontainer/{name}.log", Connection = "airflowlogs_STORAGE")]Stream myBlob, string name, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function starting processing blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function starting processing blob\n Name: {name} \n Size: {myBlob.Length} Bytes");
 
             var endpoint = new Uri(GetEnvironmentVariable("DataCollectionEndpoint"));
             var ruleId = GetEnvironmentVariable("DataCollectionRuleId");
@@ -24,17 +24,18 @@ namespace Sample.Function
             var credential = new DefaultAzureCredential();
             LogsIngestionClient client = new(endpoint, credential);
 
-            DateTimeOffset currentTime = DateTimeOffset.UtcNow;
+            var currentTime = DateTimeOffset.UtcNow;
 
             var entries = new List<Object>();
             using var reader = new StreamReader(myBlob);
 
             while (!reader.EndOfStream)
             {
-                string line = await reader.ReadLineAsync();
+                var line = await reader.ReadLineAsync().ConfigureAwait(false);
                 log.LogInformation(line);
                 entries.Add(
-                    new {
+                    new
+                    {
                         Time = currentTime,
                         RawData = line,
                         Application = "LogsIngestionFunction"
@@ -42,29 +43,20 @@ namespace Sample.Function
                 );
             }
 
-
-            // Set concurrency and EventHandler in LogsUploadOptions
-            LogsUploadOptions options = new LogsUploadOptions();
-            options.MaxConcurrency = 10;
-            options.UploadFailed += Options_UploadFailed;
-
-            await client.UploadAsync(ruleId, streamName, entries);
+            try
+            {
+                await client.UploadAsync(ruleId, streamName, entries).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Error while uploading");
+                throw;
+            }
         }
 
         private static string GetEnvironmentVariable(string name)
         {
             return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
-        }
-
-        private Task Options_UploadFailed(LogsUploadFailedEventArgs e)
-        {
-            // Throw exception from EventHandler to stop Upload if there is a failure
-            // 413 status is RequestTooLarge - don't throw here because other batches can successfully upload
-            if ((e.Exception is RequestFailedException) && (((RequestFailedException)e.Exception).Status != 413))
-                // log.LogInformation($"Total logs failed to upload: {e.FailedLogs.Count}");
-                throw e.Exception;
-            else
-                return Task.CompletedTask;
         }
     }
 }
