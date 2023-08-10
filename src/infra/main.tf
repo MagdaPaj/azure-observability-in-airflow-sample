7132bdac8affc72899f0e498856a77f59d8f292b
Magdalena Baran <mbaran@microsoft.com>
2023-08-10 18:11:53 +0200

    Deploy Log Ingestion function through terraform

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -11,7 +11,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "random_id" "random_name" {
@@ -22,6 +26,11 @@ variable "location" {
   description = "Azure region where resources will be deployed."
   type        = string
   default     = "West Europe"
+}
+
+variable "function_project_path" {
+  type    = string
+  default = "../log-ingestion"
 }
 
 resource "azurerm_resource_group" "resource_group" {
@@ -44,16 +53,17 @@ module "azure_monitor" {
 }
 
 module "logs_ingestion_function" {
-  source                                     = "./logs-ingestion-function"
-  rg_name                                    = azurerm_resource_group.resource_group.name
-  location                                   = var.location
-  random_id                                  = random_id.random_name.hex
-  raw_logs_storage_account_id                = module.raw_airflow_logs_storage.airflow_logs_storage_account_id
-  raw_logs_storage_account_name              = module.raw_airflow_logs_storage.airflow_logs_storage_account_name
-  log_analytics_workspace_id                 = module.azure_monitor.log_analytics_workspace_id
-  data_collection_endpoint                   = module.azure_monitor.data_collection_endpoint
-  data_collection_rule_immutable_id          = module.azure_monitor.data_collection_rule_immutable_id
-  data_collection_rule_id                    = module.azure_monitor.data_collection_rule_id
+  source                            = "./logs-ingestion-function"
+  rg_name                           = azurerm_resource_group.resource_group.name
+  location                          = var.location
+  random_id                         = random_id.random_name.hex
+  raw_logs_storage_account_id       = module.raw_airflow_logs_storage.airflow_logs_storage_account_id
+  raw_logs_storage_account_name     = module.raw_airflow_logs_storage.airflow_logs_storage_account_name
+  log_analytics_workspace_id        = module.azure_monitor.log_analytics_workspace_id
+  data_collection_endpoint          = module.azure_monitor.data_collection_endpoint
+  data_collection_rule_immutable_id = module.azure_monitor.data_collection_rule_immutable_id
+  data_collection_rule_id           = module.azure_monitor.data_collection_rule_id
+  function_project_path             = var.function_project_path
 }
 
 output "AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER" {
